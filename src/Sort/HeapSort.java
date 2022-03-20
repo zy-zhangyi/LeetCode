2c251e8158ec54157875087ae192338e87dec171
zy-zhangyi <994287042@qq.com>
2022-03-20 18:45:14 +0800

    update day03

---
+++ src/Sort/HeapSort.java
@@ -0,0 +1,47 @@
+package Sort;
+
+public class HeapSort {
+    public static void heapSort(int[] arr){
+        if (arr == null || arr.length < 2){
+            return;
+        }
+        for (int i = 0; i < arr.length; i++){ //O(N)
+            heapInsert(arr, i); //O(logN)
+        }
+        int heapSize = arr.length;
+        swap(arr, 0, --heapSize);
+        while (heapSize > 0){ // O(N)
+            heapify(arr, 0, heapSize); // O(logN)
+            swap(arr, 0, -- heapSize); // O(1)
+        }
+    }
+
+    public static void heapInsert(int[] arr, int index){
+        while (arr[index] > arr[(index - 1) / 2]){
+            swap(arr, index, (index - 1) / 2);
+            index = (index - 1) / 2;
+        }
+    }
+
+    public static void heapify(int[] arr, int index, int heapSize){
+        int left = index * 2 + 1;//左孩子的下标
+        while (left < heapSize){ // 下方还有孩子的时候
+            // 两个孩子中谁的值大，把其下标赋值给largest
+            int largest = left + 1 < heapSize && arr[left] < arr[left + 1] ? left + 1 : left;
+            // 父亲和孩子哪个值大，将其下标赋值给largest
+            largest = arr[index] < arr[largest] ? largest : index;
+            if (largest == index){
+                break;
+            }
+            swap(arr, index, largest);
+            index = largest;
+            left = index * 2 + 1;
+        }
+    }
+
+    public static void swap(int[] arr, int L, int R){
+        int temp = arr[L];
+        arr[L] = arr[R];
+        arr[R] = temp;
+    }
+}
---
+++ src/Sort/RadixSort.java
@@ -0,0 +1,60 @@
+package Sort;
+
+//基数排序
+public class RadixSort {
+    public static void radixSort(int[] arr){
+        if (arr == null || arr.length < 2){
+            return;
+        }
+        radixSort(arr, 0, arr.length - 1, maxbits(arr));
+    }
+
+    // 求数组最大值有几个十进制位(百位为3，千位为4)
+    public static int maxbits(int[] arr){
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++){
+            max = Math.max(max, arr[i]);
+        }
+        int res = 0;
+        while (max != 0){
+            res ++;
+            max /= 10;
+        }
+        return res;
+    }
+
+    // digit表示这组数据最大值有几个十进制位(百位为3，千位为4)
+    public static void radixSort(int[] arr, int L, int R, int digit){
+        final int radix = 10;
+        int i = 0, j = 0;
+        // 有多少个数字准备多少辅助空间
+        int[] bucket = new int[R - L + 1];
+        for (int d = 1; d <= digit; d++){ // 10个空间
+            // count[0] 当前位(d位)是0的数字有多少个
+            // count[1] 当前位(d位)是(0和1)的数字有多少个
+            // count[2] 当前位(d位)是(0,1,2)的数字有多少个
+            // count[i] 当前位(d位)是(0~i)的数字有多少个
+            int[] count = new int[radix];
+            for (i = L; i <= R; i++){
+                j = getDigit(arr[i], d);
+                count[j] ++;
+            }
+            //处理成前缀和
+            for (i = 1; i < radix; i++){
+                count[i] = count[i-1] + count[i];
+            }
+            for (i = R; i >= L; i--){
+                j = getDigit(arr[i], d);
+                bucket[count[j] - 1] = arr[i];
+                count[j] --;
+            }
+            for (i = L, j = 0; i <= R; i++, j++){
+                arr[i] = bucket[j];
+            }
+        }
+    }
+
+    public static int getDigit(int x, int d){
+        return ((x / (int) Math.pow(10, d - 1)) % 10);
+    }
+}
