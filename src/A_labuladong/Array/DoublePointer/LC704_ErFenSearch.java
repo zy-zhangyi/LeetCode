8621425a4384a41ccc0926857a97aaf0956b60b5
zy-zhangyi <994287042@qq.com>
2022-03-27 19:59:40 +0800

    update

--- src/A_labuladong/Array/DoublePointer/LC704_ErFenSearch.java
+++ src/A_labuladong/Array/DoublePointer/LC704_ErFenSearch.java
@@ -2,11 +2,12 @@ package A_labuladong.Array.DoublePointer;
 
 /*
 704. 二分查找
-给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，
+写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
  */
 
 
-public class LC704_ErFen {
+public class LC704_ErFenSearch {
     public int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
